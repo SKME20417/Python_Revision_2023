509524ff733bc1c1f673ca198169a4eb40bfa317
SKME20417 <93089937+SKME20417@users.noreply.github.com>
2023-06-03 10:02:32 +0530

    Add files via upload

---
+++ Mycustom03.py
@@ -0,0 +1,9 @@
+def greetings(name):
+    return "Hello" + name
+
+print("I will have some functionality in this modules")
+
+print("Custome Module Name:", __name__)
+
+if __name__ == "__main__":
+    print("I am having a confidential code:********")
---
+++ Project03.py
@@ -0,0 +1,9 @@
+import Mycustom03
+
+res = Mycustom03.greetings("Mohit")
+              
+print(res)
+
+print("Project Module Name:", __name__)
+                     
+                     
